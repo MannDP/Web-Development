62443d208fa3f53931a5fbd37638284456b0672f
Mann <patelmann1999@gmail.com>
2018-11-18 21:19:19 -0500

    Completed DOM Manipulation Exercises

--- Udemy_WebDevBootcamp/Part 5 DOM/Dom_Basics/Select_Exercise/selectors.js
+++ Udemy_WebDevBootcamp/Part 5 DOM/Dom_Basics/Select_Exercise/selectors.js
@@ -1,1 +1,17 @@
-let tag = document.getElementById("highlight");
+//Challenge: Find 4 ways to select the first paragraph on the page
+
+//Way 1
+let tag1 = document.getElementById("first");
+console.dir(tag1);
+
+//Way 2
+let tag2 = document.getElementsByClassName("special")[0];
+console.dir(tag2);
+
+//Way 3
+let tag3 = document.querySelector("#first");
+console.dir(tag3);
+
+//Way 4
+let tag4 = document.querySelectorAll("#first");
+console.dir(tag4);
