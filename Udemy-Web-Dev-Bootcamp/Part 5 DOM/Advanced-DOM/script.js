c4e2854859da75823c7a1fd3aa7863bc3f50b778
Mann <patelmann1999@gmail.com>
2018-11-21 23:42:04 -0500

    scalable solution

--- Udemy-Web-Dev-Bootcamp/Part 5 DOM/Advanced-DOM/script.js
+++ Udemy-Web-Dev-Bootcamp/Part 5 DOM/Advanced-DOM/script.js
@@ -1,11 +1,17 @@
-let button = document.getElementsByTagName("button")[0];
-let body = document.getElementsByTagName("body")[0];
-let isWhite = true;
+// let button = document.getElementsByTagName("button")[0];
+// let body = document.getElementsByTagName("body")[0];
+// let isWhite = true;
+// button.addEventListener("click", function() {
+//     if (isWhite) {
+//         body.style.background = "purple";
+//     } else {
+//         body.style.background = "white";
+//     }
+//     isWhite = !isWhite;
+// });
+
+//! Scalable implementation
+let button = document.querySelector("button");
 button.addEventListener("click", function() {
-    if (isWhite) {
-        body.style.background = "purple";
-    } else {
-        body.style.background = "white";
-    }
-    isWhite = !isWhite;
+    document.body.classList.toggle("purple");
 });
