5a5021d116b5f09efbcb4ccb88d3964096bd2a5c
Mann <patelmann1999@gmail.com>
2018-12-14 23:53:49 -0500

    Business logic of app complete

--- Udemy-Web-Dev-Bootcamp/Part 8 ToDoList/assets/js/todos.js
+++ Udemy-Web-Dev-Bootcamp/Part 8 ToDoList/assets/js/todos.js
@@ -1,4 +1,20 @@
 //Check off specific todos by clicking 
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
+});
+
+//Click on X and delete todo
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        const todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 }); 
