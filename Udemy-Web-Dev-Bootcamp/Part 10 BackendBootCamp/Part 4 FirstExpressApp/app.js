3286c5f27c2f1ce0d0b319d3a7d002b3b76f3148
Mann <patelmann1999@gmail.com>
2019-01-10 15:24:49 -0500

    Package.json experimenting; updating basic express app

--- Udemy-Web-Dev-Bootcamp/Part 10 BackendBootCamp/Part 4 FirstExpressApp/app.js
+++ Udemy-Web-Dev-Bootcamp/Part 10 BackendBootCamp/Part 4 FirstExpressApp/app.js
@@ -2,11 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//First routes
 app.get("/", function (req, res) {
     res.send("Hi There!");
 });
 app.get("/bye", (req, res) => res.send("Goodbye"));
 app.get("/dog", (req, res) => res.send("MEOW!"));
+
+//Route matchers
+app.get("/r/:subReddit", (req, res) => {
+    const subReddit = req.params.subReddit;
+    res.send(`Welcome to the ${subReddit} Sub-Reddit Page`);
+});
+
+// Route matcher runs on any route... therefore, it needs to be at the bottom
+app.get("*", (undefined, res) => res.send("Oops! We couldn't find the page you're looking for!"));
 
 app.listen(port, undefined, function () {
     console.log("Serving your first express app!");
